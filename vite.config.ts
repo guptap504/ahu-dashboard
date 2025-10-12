8f9f031c73e2f9d6fe738c00e9acd59638c124e9
Prashant Gupta <pgupta2596@gmail.com>
2025-10-13 04:47:10 +0530

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,20 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
+// import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
-    nitroV2Plugin(),
+    // nitroV2Plugin(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
-    viteReact(),
     netlify(),
+    viteReact(),
   ],
 })
 
